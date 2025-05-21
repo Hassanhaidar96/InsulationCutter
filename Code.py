8a93f45a4208b89ae3d1479c374cd3bded120592
Hassanhaidar96 <104452064+Hassanhaidar96@users.noreply.github.com>
2025-05-21 13:39:24 +0200

    Add files via upload

--- Code.py
+++ Code.py
@@ -24,24 +24,24 @@ st.set_page_config(
 USERNAME = "admin"
 PASSWORD = "password123"
 
+
+    
+
 st.title("Login")
     
-def login():
-    st.title("Login")
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
     
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    
-    if st.button("Login"):
-        if username == USERNAME and password == PASSWORD:
-            st.session_state.logged_in = True
-            st.rerun()
-        else:
-            st.error("Invalid username or password")
-            st.stop()  # Stops execution here
+if st.button("Login"):
+    if username == USERNAME and password == PASSWORD:
+        st.session_state.logged_in = True
+        st.rerun()
+    else:
+        st.error("Invalid username or password")
+        st.stop()  # Stops execution here
             
     # This code won't run if st.stop() is called
-    st.write("Please enter your credentials")
+st.write("Please enter your credentials")
             
 
 def adjust_h_for_fire_resistance(Cb, Ct, fire_resistance):
